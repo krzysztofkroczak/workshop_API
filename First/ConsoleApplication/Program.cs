039e123dcb519d0d9540a3794b88a26320d8560d
Krzysztof Kroczak <krzysztof.kroczak@red-gate.com>
2017-11-02 16:35:54 +0000

    New handon

--- First/ConsoleApplication/Program.cs
+++ First/ConsoleApplication/Program.cs
@@ -7,9 +7,19 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
+            var result = new NonEmptyCollection<int>(42, new[] {1337, 123});
+            Console.WriteLine("Items:");
+            foreach (var i in result)
+                Console.WriteLine("{0,9}", i);
+            Console.WriteLine("Max: {0}", Accumulate(result));
+            Console.ReadKey();
+        }
+
+        private static void First()
+        {
             var writer = new CompositeTextWriter(new ConsoleTextWriter(),
                 new FileTextWriter(@"./Messages.txt"), new NullTextWriter());
-            
+
             writer.WriteLines("Foo");
             writer.WriteLines("Bar");
             writer.WriteLines("Baz");
