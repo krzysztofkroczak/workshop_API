7d1b904d4e91c30381beb53501a2fcbf71bf874d
Krzysztof Kroczak <krzysztof.kroczak@red-gate.com>
2017-11-02 14:26:06 +0000

    extract: https://www.dropbox.com/s/ly8cncr0pav47ny/HandsOn04.zip

---
+++ HandsOn04/CompositeDateTimeOffsetAdjustment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Workshop.DPtoCT.HandsOn04
+{
+    public class CompositeDateTimeOffsetAdjustment : IDateTimeOffsetAdjustment
+    {
+        private readonly IReadOnlyCollection<IDateTimeOffsetAdjustment> adjustments;
+
+        public CompositeDateTimeOffsetAdjustment(
+            params IDateTimeOffsetAdjustment[] adjustments)
+        {
+            this.adjustments = adjustments;
+        }
+
+        public DateTimeOffset Adjust(DateTimeOffset value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
