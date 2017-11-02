5857e1e1b8212d2b497cc4c8268991020194c4df
Krzysztof Kroczak <krzysztof.kroczak@red-gate.com>
2017-11-02 16:40:45 +0000

    Implementation of task

--- First/ConsoleApplication/Program.cs
+++ First/ConsoleApplication/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using First;
 
 namespace ConsoleApplication
@@ -7,12 +10,23 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
+            //expected output:
+            // Items:
+            //42
+            //1337
+            //123
+            //Max: 1337
             var result = new NonEmptyCollection<int>(42, new[] {1337, 123});
             Console.WriteLine("Items:");
             foreach (var i in result)
                 Console.WriteLine("{0,9}", i);
             Console.WriteLine("Max: {0}", Accumulate(result));
             Console.ReadKey();
+        }
+
+        private static int Accumulate(NonEmptyCollection<int> result)
+        {
+            return result.Aggregate(Math.Max);
         }
 
         private static void First()
@@ -28,6 +42,27 @@ namespace ConsoleApplication
             writer.WriteLines("Fnahh");
 
             Console.ReadKey();
+        }
+    }
+
+    internal class NonEmptyCollection<T> : IEnumerable<T>
+    {
+        private readonly IList<T> col;
+
+        public NonEmptyCollection(T i, T[] ints)
+        {
+            col = ints.ToList();
+            col.Insert(0, i);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return col.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
