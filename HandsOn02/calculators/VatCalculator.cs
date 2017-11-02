f33be3401ced7674d8cc4fcecf2d9452cfd4282e
Krzysztof Kroczak <krzysztof.kroczak@red-gate.com>
2017-11-02 11:51:11 +0000

    Use LINQ in VatCalculator

--- HandsOn02/calculators/VatCalculator.cs
+++ HandsOn02/calculators/VatCalculator.cs
@@ -1,4 +1,6 @@
-﻿namespace Ploeh.Workshop.DPtoCT.HandsOn02.calculators
+﻿using System.Linq;
+
+namespace Ploeh.Workshop.DPtoCT.HandsOn02.calculators
 {
     public class VatCalculator : IPriceCalculator
     {
@@ -11,14 +13,9 @@
 
         public decimal CalculatePrice(Basket basket)
         {
-            var vat = 0m;
-            foreach (var p in basket)
-            {
-                // Not VAT on newspapers, in order to subsidise a free press.
-                if (p.Name != "Newspaper")
-                    vat = vat + p.Price * this.vatRate;
-            }
-            return vat;
+            // Not VAT on newspapers, in order to subsidise a free press.
+            return basket.Where(p => p.Name != "Newspaper")
+                .Sum(p => p.Price * vatRate);
         }
     }
 }
