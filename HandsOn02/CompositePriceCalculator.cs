2c4212993542c99af6b8067026eee31eb21a6c18
Krzysztof Kroczak <krzysztof.kroczak@red-gate.com>
2017-11-02 11:38:26 +0000

    Value should be 66.00 (implemented) :)

--- HandsOn02/CompositePriceCalculator.cs
+++ HandsOn02/CompositePriceCalculator.cs
@@ -8,13 +8,16 @@ namespace Ploeh.Workshop.DPtoCT.HandsOn02
 {
     public class CompositePriceCalculator : IPriceCalculator
     {
+        private readonly IPriceCalculator[] _calculators;
+
         public CompositePriceCalculator(params IPriceCalculator[] calculators)
         {
+            _calculators = calculators;
         }
 
         public decimal CalculatePrice(Basket basket)
         {
-            throw new NotImplementedException();
+            return _calculators.Select(c=>c.CalculatePrice(basket)).Sum();
         }
     }
 }
